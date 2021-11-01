40f93ce64c740873e3497575ffe7e413684560b8
Veronika <verona.binko@gmail.com>
2021-11-01 11:32:39 +0300

    Feat: add cart-context and use useContext HeaderCartButton component

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,25 @@
-import React, { Fragment } from 'react';
+import React, { useContext } from 'react';
 
+import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
-  return <Fragment>
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
+  return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
-  </Fragment>
+  )
 };
 
 export default HeaderCartButton;
