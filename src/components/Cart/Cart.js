e40cf93ede3af150ffbc96fdbf482c23f1623c49
Veronika <verona.binko@gmail.com>
2021-11-01 10:08:58 +0300

    Feat: add Modal, use Portal, make Modal close using button'close' and click on backdrop

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import Modal from '../UI/Modal';
+import classes from './Cart.module.css';
+
+const Cart = props => {
+
+  const cartItems = <ul className={classes['cart-items']}>{
+    [{
+      id: '1c', 
+      name: 'Sushi', 
+      amount: 2, 
+      price: 15.6
+    }]
+      .map(item => 
+      <li>{item.name}</li>)}
+    </ul>
+
+  return (
+  <Modal onClose={props.onClose}>
+    {cartItems}
+    <div className={classes.total}>
+      <span>Total Amount</span>
+      <span>35.66</span>
+    </div>
+    <div className={classes.actions}>
+      <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
+      <button className={classes.button}>Order</button>
+    </div>
+  </Modal>
+  )
+};
+
+export default Cart;
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = (props) => {
   return <Fragment>
     <header className={classes.header}>
       <h1>My Meals</h1>
-      <HeaderCartButton />
+      <HeaderCartButton onClick={props.onShowCart} />
     </header>
     <div className={classes['main-image']}>
       <img src={mealsImg} alt="A table with food" />
